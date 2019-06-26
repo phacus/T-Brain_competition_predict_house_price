78e705cb75c4b929e42a2da8ad478d5765bb086e
Phacus <kos147258@gmail.com>
2019-06-26 16:08:55 +0800

    New LightGBM model (python script) casual score ~5600

--- LightGBM_origin.py
+++ LightGBM_origin.py
@@ -11,96 +11,123 @@ from sklearn.datasets import  make_classification
 
 import time
 
-print('Loading data...')
+def lgb_train(random_state):
+    print('Loading data...')
 
-# 讀檔
-train = pd.read_csv('dataset-0510/train.csv')
+    # 讀檔
+    # train = pd.read_csv('dataset-0510/train.csv')
+    train = pd.read_csv('FE_train.csv')
+    del train['building_id']
 
-# 設定ignored list(cus training dataset can not be zero or NaN) without Feature Enginnearing
-# FE later
-ignored = ['parking_price', 'parking_area', 'txn_floor', 'village_income_median', 'building_id']
-train = train.drop(columns=ignored)
+    # 設定ignored list(cus training dataset can not be zero or NaN) without Feature Enginnearing
+    # FE later
+    # ignored = ['parking_price', 'parking_area', 'txn_floor', 'village_income_median', 'building_id']
+    # train = train.drop(columns=ignored)
 
-# 對目標欄位進行處理(cus price range too large)
-train['total_price'] = np.log1p(train['total_price'])
+    # 對目標欄位進行處理(cus price range too large)
+    train['total_price'] = np.log1p(train['total_price'])
 
-# 設定train, test datasets
-y = train['total_price']
-del train['total_price']
-X = train.values
-y = y.values
-X_train,X_test,y_train,y_test =train_test_split(X, y, test_size=0.2)
+    # 設定train, test datasets
+    y = train['total_price']
+    del train['total_price']
+    X = train.values
+    y = y.values
 
-# lgb dataset
-lgb_train = lgb.Dataset(X_train, y_train)
-lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+    # 分割dataset為test, train
+    X_train,X_test,y_train,y_test =train_test_split(X, y, test_size=0.2, random_state=random_state)
+    
+    X_test_1 = X_test[:6000]
+    y_test_1 = y_test[:6000]
+    X_test_2 = X_test[6000:]
+    y_test_2 = y_test[6000:]
 
-# custom metric function
-def c_hit(preds, train_data):
-    trues = train_data.get_label()
-    scores = np.absolute((preds - trues)/trues)
-    return 'hit', np.sum(scores), False
+    # lgb dataset
+    lgb_train = lgb.Dataset(X_train, y_train)
+    lgb_eval_1 = lgb.Dataset(X_test_1, y_test_1, reference=lgb_train)
+    lgb_eval_2 = lgb.Dataset(X_test_2, y_test_2, reference=lgb_train)
 
-print('Start training...')
+    # custom metric function
+    def c_hit(preds, train_data):
+        trues = train_data.get_label()
+        trues = np.expm1(trues)
+        preds = np.expm1(preds)
+        scores = np.absolute((preds - trues)/trues) > 0.1
+        hit_error = np.sum(scores)/train_data.num_data()
+        return 'hit_error', hit_error, False
 
-start_time = time.time()
+    print('Start training...')
 
-# Model參數
-params = {
-    'task': 'train',
-    'boosting_type': 'gbdt',
-    'objective': 'regression',
-    'metric': "None",
-    'num_leaves': 31,
-    'learning_rate': 0.1,
-    'feature_fraction': 0.9,
-    'bagging_fraction': 0.8,
-    'bagging_freq': 5,
-    'verbose': 1,
-    'num_threads': -1,
-}
+    # Model參數
+    params = {
+        'task': 'train',
+        'boosting_type': 'gbdt',
+        'objective': 'regression',
+        'metric': "None",
+        'num_leaves': 31,
+        # 'max_bin': 512,
+        'learning_rate': 0.05,
+        # 'min_data_in_leaf': 100,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.7,
+        'bagging_freq': 10,
+        'verbose': 1,
+        'num_threads': -1,
+    }
 
-# params ={
-#         'n_estimators': 10000, 'max_depth' : -1, 'num_leaves' :30,         
-#         'objective': 'regression',   'metric': 'rmse',   
-#         'learning_rate': 0.01,      'boosting': 'gbdt',     'min_data_in_leaf': 10,
-#         'feature_fraction': 0.9,    'bagging_freq':1,       'bagging_fraction': 0.8,     'importance_type': 'gain',
-#         'lambda_l1': 0.2,  'subsample': .8,   'colsample_bytree': .9, 'num_threads' : -1
-#     }
+    # CV and training
 
-# CV and training
-gbm = lgb.train(
-    params, 
-    lgb_train, 
-    num_boost_round=1000, 
-    valid_sets=lgb_eval, 
-    early_stopping_rounds=1000,
-    # fobj=loglikelihood,
-    feval=lambda preds, train_data: [c_hit(preds, train_data)],
-    verbose_eval=500,
-)
+    start_time = time.time()
 
-# print('Saving model...')
-# gbm.save_model('model.txt')
+    gbm = lgb.train(
+        params, 
+        lgb_train, 
+        num_boost_round=10000, 
+        valid_sets=[lgb_eval_1, lgb_eval_2], 
+        early_stopping_rounds=1000,
+        feval=lambda preds, train_data: [c_hit(preds, train_data)],
+        verbose_eval=100,
+    )
 
-end_time = time.time()
+    # print('Saving model...')
+    # gbm.save_model('model.txt')
 
-print('Start predicting...')
+    end_time = time.time()
 
-# Prediting
-y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+    print('Start predicting...')
 
-# Validating
-# 還原目標欄位
-y_pred = np.expm1(y_pred)
-y_test = np.expm1(y_test)
+    # Prediting
+    y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
 
-# 計算hit rate, MAPE
-hit = np.absolute((y_test - y_pred)/y_test)
-hit_rate = np.sum(hit < 0.1) / len(hit)
-MAPE = np.sum(hit)/len(hit)
+    # Validating
+    # 還原目標欄位
+    y_pred = np.expm1(y_pred)
+    y_test = np.expm1(y_test)
 
-print('MAPE: ', MAPE)
-print('Hit Rate: ', hit_rate * 100,'%')
-print('Score: ', hit_rate*(10**4) + (1 - MAPE))
-print('Training Time: ', end_time - start_time, 's')
+    # 計算hit rate, MAPE
+    hit = np.absolute((y_test - y_pred)/y_test)
+    hit_rate = np.sum(hit < 0.1) / len(hit)
+    MAPE = np.sum(hit)/len(hit)
+
+    print('MAPE: ', MAPE)
+    print('Hit Rate: ', hit_rate * 100,'%')
+    print('Score: ', hit_rate*(10**4) + (1 - MAPE))
+    print('Training Time: ', end_time - start_time, 's')
+
+    # 參數寫入log檔
+    # f = open('model_training_log.txt', 'a')
+    # log = '\nRandom state: ' + str(random_state) + ', Scores: ' + str(hit_rate*(10**4) + (1 - MAPE))
+    # f.write(log)
+    # f.close()
+
+    # Create submission csv file
+    test = pd.read_csv('FE_test.csv')
+    names = test['building_id']
+    del test['building_id']
+    del test['total_price']
+    y_out = gbm.predict(test, num_iteration=gbm.best_iteration)
+    test['total_price'] = np.expm1(y_out)
+    test['building_id'] = names
+    file_name = 'output_' + str(time.strftime("%Y%m%d%H%M%S", time.localtime())) + '.csv'
+    test[['building_id', 'total_price']].to_csv(file_name, index=False)
+
+lgb_train(11)
--- LightGBM_sklearn.py
+++ LightGBM_sklearn.py
@@ -4,38 +4,54 @@
 import lightgbm as lgb
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
 
 print('Loading data...')
 
 # 讀檔
-train = pd.read_csv('dataset-0510/train.csv')
+train = pd.read_csv('FE_train.csv')
 
 # 設定ignored list(cus training dataset can not be zero or NaN) without Feature Enginnearing
 # FE later
-ignored = ['parking_price', 'parking_area', 'txn_floor', 'village_income_median', 'building_id']
-train = train.drop(columns=ignored)
+# ignored = ['parking_price', 'parking_area', 'txn_floor', 'village_income_median', 'building_id']
+# train = train.drop(columns=ignored)
 
 # 對目標欄位進行處理(cus price range too large)
 train['total_price'] = np.log1p(train['total_price'])
+del train['building_id']
 
 # 設定train, test datasets
 y = train['total_price']
 del train['total_price']
 X = train.values
 y = y.values
+
 X_train,X_test,y_train,y_test =train_test_split(X, y, test_size=0.2)
 
 print('Start training...')
 
 def c_hit(y_true, y_pred):
-    return 'hit', np.sum(np.abs((y_pred - y_true)/y_true)), False
+    y_true = np.expm1(y_true)
+    y_pred = np.expm1(y_pred)
+    scores = np.abs((y_pred - y_true)/y_true) > 0.1
+    hit_error = np.sum(scores)/len(y_true)
+    return 'hit_error', hit_error, False
 
 # Training
-gbm = lgb.LGBMRegressor(objective='regression', num_leaves=31, learning_rate=0.1, n_estimators=1000, n_jobs=-1, metric="None")
-gbm.fit(X_train, y_train, eval_set=[(X_test, y_test)], eval_metric=lambda y_true, y_pred: [c_hit(y_true, y_pred)], early_stopping_rounds=5)
+gbm = lgb.LGBMRegressor(
+    objective='regression', 
+    num_leaves=31, 
+    learning_rate=0.1, 
+    n_estimators=1000, 
+    n_jobs=-1, 
+    metric="None"
+)
+gbm.fit(X_train, y_train, 
+    eval_set=[(X_test, y_test)], 
+    eval_metric=lambda y_true, y_pred: [c_hit(y_true, y_pred)], 
+    early_stopping_rounds=100
+)
 
 print('Start predicting...')
 
@@ -55,19 +71,3 @@ MAPE = np.sum(hit)/len(hit)
 print('MAPE: ', MAPE)
 print('Hit Rate: ', hit_rate * 100,'%')
 print('Score: ', hit_rate*(10**4) + (1 - MAPE))
-
-# Feature Importances
-# print('Feature importances: ', list(gbm.feature_importances_))
-
-# 尋找最佳化參數
-# estimator = lgb.LGBMRegressor(num_leaves=31)
-
-# param_grid = {
-#     'learning_rate': [0.01, 0.1, 1],
-#     'n_estimators': [100, 1000, 10000],
-# }
-
-# gbm = GridSearchCV(estimator, param_grid, verbose=2, n_jobs=4)
-# gbm.fit(X_train, y_train)
-
-# print('Best parameters: ', gbm.best_params_)
